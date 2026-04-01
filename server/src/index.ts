6fff4b10e62a38c4fbc9ee157ab3eda9825fcf35
dipudangol2 <dipudangol2@gmail.com>
2026-04-01 21:15:25 +0545

    Chore(server): Boilerplate setup for the server.

---
+++ server/src/index.ts
@@ -0,0 +1,27 @@
+import express, { Application } from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import dotenv from "dotenv";
+
+dotenv.config();
+const port = process.env.PORT || 8000;
+
+const app: Application = express();
+app.use(
+    cors({
+        origin: "http://localhost:5173",
+        methods: ["GET", "PUT", "POST", "DELETE","PATCH"],
+        credentials: true
+    })
+)
+
+app.use(cookieParser());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.send("Hello World!");
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
