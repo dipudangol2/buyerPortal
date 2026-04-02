a0129e62bbd6ee21f74647fd637d31ad071ed435
dipudangol2 <dipudangol2@gmail.com>
2026-04-02 21:43:01 +0545

    feat(Docker): Containerized the application with multi-container setup with docker compose.

--- server/src/index.ts
+++ server/src/index.ts
@@ -12,7 +12,7 @@ const port = process.env.PORT || 8000;
 const app: Application = express();
 app.use(
     cors({
-        origin: "http://localhost:5173",
+        origin: "*",
         methods: ["GET", "PUT", "POST", "DELETE", "PATCH"],
         credentials: true
     })
